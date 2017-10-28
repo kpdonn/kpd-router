1ec9aeb3e35778a187d74f48fcaa0ed188d31417
Kevin Donnelly <kdonnelly08@gmail.com>
2017-10-28 00:33:12 -0500

    Add initial implementation and a couple tests.

--- src/typed-mobx-router.ts
+++ src/typed-mobx-router.ts
@@ -1,4 +1,102 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+import RouteNode from 'route-node'
+
+import { autorun, computed, observable, runInAction } from 'mobx'
+import { History } from 'history'
+
+export interface RouterBuilder<T> {
+  start(): T & { routerStore: RouterStore }
+
+  addRoute<
+    L extends (params: P) => void,
+    P extends { [name: string]: string },
+    N extends string
+  >(
+    route: Route<L, P, N>
+  ): RouterBuilder<T & Record<N, L>>
+}
+
+export interface Route<
+  L extends (params: P) => void,
+  P extends { [name: string]: string },
+  N extends string
+> {
+  name: N
+  path: string
+  onLoad: L
+}
+
+export function newRouter(history: History): RouterBuilder<{}> {
+  return new RouterBuilderImpl(history)
+}
+
+class RouterBuilderImpl<T = {}> implements RouterBuilder<T> {
+  private router: any = { routerStore: new RouterStore() }
+
+  constructor(readonly history: History) {}
+
+  start(): T & { routerStore: RouterStore } {
+    const routerStore = this.router.routerStore
+
+    const match = routerStore.rootNode.matchPath(
+      this.history.location.pathname + this.history.location.search
+    )
+    if (match) {
+      this.router[match.name](match.params)
+    }
+
+    this.history.listen((newLocation, action) => {
+      if (action !== 'POP') {
+        return
+      }
+      const match = routerStore.rootNode.matchPath(
+        newLocation.pathname + newLocation.search
+      )
+      if (match) {
+        this.router[match.name](match.params)
+      }
+    })
+
+    autorun(() => {
+      const path = routerStore.currentPath
+      if (
+        path !==
+        this.history.location.pathname + this.history.location.search
+      ) {
+        this.history.push(path)
+      }
+    })
+
+    return this.router as T & { routerStore: RouterStore }
+  }
+
+  addRoute<
+    L extends (params: P) => void,
+    P extends { [name: string]: string },
+    N extends string
+  >(route: Route<L, P, N>): RouterBuilder<T & Record<N, L>> {
+    const routerStore = this.router.routerStore
+    routerStore.rootNode.addNode(route.name, route.path)
+    this.router[route.name] = function(args: P) {
+      runInAction(() => {
+        routerStore.routeName = route.name
+        routerStore.params = args
+        route.onLoad(args)
+      })
+    }
+
+    return this as any
+  }
+}
+
+export class RouterStore {
+  @observable routeName: string
+
+  @observable params: object
+
+  rootNode = new RouteNode()
+
+  @computed
+  get currentPath(): string {
+    return this.rootNode.buildPath(this.routeName, this.params)
+  }
+}
--- test/typed-mobx-router.test.ts
+++ test/typed-mobx-router.test.ts
@@ -1,14 +1,51 @@
-import DummyClass from '../src/typed-mobx-router'
+import { newRouter } from '../src/typed-mobx-router'
+import createMemoryHistory from 'history/createMemoryHistory'
 
-/**
- * Dummy test
- */
-describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
+const history = createMemoryHistory()
+const mainOnLoad = jest.fn()
+const peopleOnLoad = jest.fn()
+const personOnLoad = jest.fn()
+
+const router = newRouter(history)
+  .addRoute({
+    name: 'main',
+    path: '/',
+    onLoad: () => mainOnLoad()
+  })
+  .addRoute({
+    name: 'people',
+    path: '/people',
+    onLoad: () => peopleOnLoad()
+  })
+  .addRoute({
+    name: 'person',
+    path: '/people/:id',
+    onLoad: (params: { id: string }) => personOnLoad(params)
+  })
+  .start()
+
+describe('Router test', () => {
+  it('is on main page to start', () => {
+    expect(router.routerStore.routeName).toBe('main')
+    expect(mainOnLoad).toBeCalled()
   })
 
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  it('goes to people when function called', () => {
+    expect(peopleOnLoad).not.toBeCalled()
+    router.people()
+    expect(router.routerStore.routeName).toBe('people')
+    expect(peopleOnLoad).toBeCalled()
+    expect(history.location.pathname + history.location.search).toBe('/people')
+  })
+
+  it('goes to person when function called', () => {
+    expect(personOnLoad).not.toBeCalled()
+    router.person({ id: '42' })
+    expect(router.routerStore.routeName).toEqual('person')
+    expect(router.routerStore.params).toEqual({ id: '42' })
+    expect(personOnLoad).toBeCalledWith({ id: '42' })
+    expect(history.location.pathname + history.location.search).toBe(
+      '/people/42'
+    )
   })
 })
