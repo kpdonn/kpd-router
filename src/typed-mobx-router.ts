0683de6656baa9d0f149f346fb9cfb2a8b40e38b
Kevin Donnelly <kdonnelly08@gmail.com>
2017-11-08 03:01:14 -0600

    Start implementing.

--- src/typed-mobx-router.ts
+++ src/typed-mobx-router.ts
@@ -1,5 +1,8 @@
 import { History } from "history"
-import { RouterBuilder } from "interfaces"
+import { RouterBuilder, RouterStore } from "interfaces"
+import * as React from "react"
+import * as RouteNode from "route-node"
+import { computed, observable, runInAction } from "mobx"
 
 export const path = <T extends string>(
   literals: TemplateStringsArray,
@@ -7,12 +10,65 @@ export const path = <T extends string>(
 ): [string, T[]] => {
   const builtPath = literals
     .map((litString, i) => (args[i] ? litString + ":" + args[i] : litString))
-    .join()
+    .join("")
 
   return [builtPath, args]
 }
 
 export function newRouter(history: History): RouterBuilder {
   // return new RouterBuilderImpl(history)
-  return { history } as any
+  return new RouterBuilderImpl(history) as any
+}
+
+class RouterStoreImpl
+  implements RouterStore<{ [index: string]: Function }, any, { route: string; params: any }> {
+  goTo: { [index: string]: Function } = {}
+  Link: React.ComponentClass<any>
+
+  @observable currentRoute: { route: string; params: any }
+
+  constructor(private rootNode: any) {}
+
+  @computed
+  get currentPath(): string {
+    return this.rootNode.buildPath(this.currentRoute.route, this.currentRoute.params)
+  }
+}
+
+class RouterBuilderImpl {
+  private rootNode = new RouteNode()
+  private goTo: { [index: string]: Function } = {}
+
+  private routerStore = new RouterStoreImpl(this.rootNode)
+
+  constructor(public history: History) {}
+
+  start(): RouterStoreImpl {
+    return {} as any
+  }
+
+  addRoute(route: Route): this {
+    if (typeof route.path === "string") {
+      this.rootNode.addNode(route.name, route.path)
+    } else {
+      this.rootNode.addNode(route.name, route.path[0])
+    }
+
+    this.goTo[route.name] = (...args: any[]) => {
+      runInAction(() => {
+        this.routerStore.currentRoute = { route: route.name, params: args }
+      })
+    }
+    return this
+  }
+}
+
+interface Route {
+  name: string
+  path: [string, string[]] | string
+  queryParams?: string[]
+  onLoad?: (...args: any[]) => void
+  defaults?: { [index: string]: any }
+  converters?: { names: string[]; from: (arg: any) => string }[]
+  component?: React.ComponentType
 }
