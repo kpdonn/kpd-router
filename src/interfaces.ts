50e413bce573f6f47af65a33ed0caa54a0d41a66
Kevin Donnelly <kdonnelly08@gmail.com>
2017-11-09 06:42:17 -0600

    If all required params have default, don't require empty object in goto.

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -27,6 +27,85 @@ export interface RouterBuilder<T = {}, L = never, S = never> {
     CT4,
     CN5 extends string,
     CT5,
+    D extends DefaultsAllReq<R, Q, CN0, CT0, CN1, CT1, CN2, CT2, CN3, CT3, CN4, CT4, CN5, CT5>
+  >(route: {
+    name: N
+    path: [string, R[]]
+    queryParams?: Q[]
+    onLoad?: (
+      params: OnLoadParams<R, Q, D, CN0, CT0, CN1, CT1, CN2, CT2, CN3, CT3, CN4, CT4, CN5, CT5>
+    ) => void
+    defaults: D
+    converters?: Converters<CN0, CT0, CN1, CT1, CN2, CT2, CN3, CT3, CN4, CT4, CN5, CT5>
+    component?: ReactComponentCreator<
+      OnLoadParams<R, Q, D, CN0, CT0, CN1, CT1, CN2, CT2, CN3, CT3, CN4, CT4, CN5, CT5>
+    >
+  }): RouterBuilder<
+    T &
+      Record<
+        N,
+        (
+          args?: GoToRouteParams<
+            R,
+            Q,
+            D,
+            CN0,
+            CT0,
+            CN1,
+            CT1,
+            CN2,
+            CT2,
+            CN3,
+            CT3,
+            CN4,
+            CT4,
+            CN5,
+            CT5
+          >
+        ) => void
+      >,
+    | L
+    | ({ route: N } & GoToRouteParams<
+      R,
+      Q,
+      D,
+      CN0,
+      CT0,
+      CN1,
+      CT1,
+      CN2,
+      CT2,
+      CN3,
+      CT3,
+      CN4,
+      CT4,
+      CN5,
+      CT5
+    >),
+    | S
+    | ({
+      route: N
+      params: Readonly<
+        OnLoadParams<R, Q, D, CN0, CT0, CN1, CT1, CN2, CT2, CN3, CT3, CN4, CT4, CN5, CT5>
+      >
+    })
+  >
+  addRoute<
+    N extends string,
+    R extends string,
+    Q extends string,
+    CN0 extends string,
+    CT0,
+    CN1 extends string,
+    CT1,
+    CN2 extends string,
+    CT2,
+    CN3 extends string,
+    CT3,
+    CN4 extends string,
+    CT4,
+    CN5 extends string,
+    CT5,
     D extends Defaults<R, Q, CN0, CT0, CN1, CT1, CN2, CT2, CN3, CT3, CN4, CT4, CN5, CT5>
   >(route: {
     name: N
@@ -123,6 +202,36 @@ export type MultiDiff<
   M6 extends string = string,
   M7 extends string = string
 > = Diff<Diff<Diff<Diff<Diff<Diff<Diff<A, M1>, M2>, M3>, M4>, M5>, M6>, M7>
+
+export type DefaultsAllReq<
+  R extends string,
+  Q extends string,
+  CN0 extends string,
+  CT0,
+  CN1 extends string,
+  CT1,
+  CN2 extends string,
+  CT2,
+  CN3 extends string,
+  CT3,
+  CN4 extends string,
+  CT4,
+  CN5 extends string,
+  CT5
+> = ReqParams<MultiDiff<R, CN0, CN1, CN2, CN3, CN4, CN5>> &
+  ReqParams<Diff<CN0, Q>, CT0> &
+  ReqParams<Diff<CN1, Q>, CT1> &
+  ReqParams<Diff<CN2, Q>, CT2> &
+  ReqParams<Diff<CN3, Q>, CT3> &
+  ReqParams<Diff<CN4, Q>, CT4> &
+  ReqParams<Diff<CN5, Q>, CT5> &
+  OptParams<MultiDiff<Q, CN0, CN1, CN2, CN3, CN4, CN5>> &
+  OptParams<Diff<CN0, R>, CT0> &
+  OptParams<Diff<CN1, R>, CT1> &
+  OptParams<Diff<CN2, R>, CT2> &
+  OptParams<Diff<CN3, R>, CT3> &
+  OptParams<Diff<CN4, R>, CT4> &
+  OptParams<Diff<CN5, R>, CT5>
 
 export type Defaults<
   R extends string,
