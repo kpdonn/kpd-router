b38d8df97ba86fd2678be2a1b4d5fc90dad03822
Kevin Donnelly <kdonnelly08@gmail.com>
2018-03-02 04:21:52 -0600

    Update way type parameters are handled.

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -13,39 +13,58 @@ export type RouteState<N extends string, OnLoadArgs> = {
   params: Readonly<OnLoadArgs>
 }
 
+export type Props<Req extends string, Opt extends string> = Literal<Req> | Literal<Opt>
+
+export type PropTypes<R extends string, Q extends string, PC extends Converters<Props<R, Q>>> = {
+  [P in Props<R, Q>]: P extends keyof PC ? (PC[P] extends Converter<infer T> ? T : never) : string
+}
+
+export type LoadArgs<
+  R extends string,
+  Q extends string,
+  PC extends Converters<Props<R, Q>>,
+  D
+> = PartPartial<PropTypes<R, Q, PC>, Exclude<Literal<Q>, Literal<keyof D>>>
+
 export interface RouterBuilder<T = {}, L = never, S = never> {
   start(): RouterStore<T, L, S>
 
   addRoute<
-    Name extends string,
-    RawReq extends string,
-    RawOpt extends string,
-    Req extends Literal<RawReq>,
-    Opt extends Literal<RawOpt>,
-    Params extends Req | Opt,
-    Conv extends Exactly<Converters<Params>, Conv>,
-    Converted extends { [P in Params]: Conv[P] extends Converter<infer T> ? T : string },
-    Defaults extends Exactly<Partial<Converted>, Defaults>,
-    GoToArgs extends PartPartial<Converted, Opt | Literal<keyof Defaults>>,
-    OnLoadArgs extends PartPartial<Converted, Exclude<Opt, Literal<keyof Defaults>>>
+    N extends string,
+    R extends string,
+    Q extends string,
+    PC extends Exactly<Converters<Props<R, Q>>, PC>,
+    D extends Exactly<Partial<PropTypes<R, Q, PC>>, D>
   >(route: {
-    name: Name
-    path: [string, RawReq[]] | string
-    queryParams?: RawOpt[]
-    onLoad?: (arg: OnLoadArgs) => void
-    defaults?: Defaults
-    converters?: Conv
-    component?: ReactComponentCreator<OnLoadArgs>
+    name: N
+    path: [string, R[]] | string
+    queryParams?: Q[]
+    onLoad?: (arg: LoadArgs<R, Q, PC, D>) => void
+    defaults?: D
+    converters?: PC
+    component?: ReactComponentCreator<LoadArgs<R, Q, PC, D>>
   }): RouterBuilder<
-    T & GoToFun<Name, GoToArgs>,
-    L | ({ route: Name } & GoToArgs),
-    S | RouteState<Name, OnLoadArgs>
+    T & GoToFun<N, R, Q, PC, D>,
+    L | ({ route: N } & GoToFunArgs<R, Q, PC, D>),
+    S | RouteState<N, LoadArgs<R, Q, PC, D>>
   >
 }
+export type GoToFunArgs<
+  R extends string,
+  Q extends string,
+  PC extends Converters<Props<R, Q>>,
+  D
+> = PartPartial<PropTypes<R, Q, PC>, Literal<Q> | Literal<keyof D>>
 
-export type GoToFun<Name extends string, Args> = {} extends Args
-  ? Record<Name, (arg?: Args) => void>
-  : Record<Name, (arg: Args) => void>
+export type GoToFun<
+  N extends string,
+  R extends string,
+  Q extends string,
+  PC extends Converters<Props<R, Q>>,
+  D
+> = {} extends GoToFunArgs<R, Q, PC, D>
+  ? Record<N, (arg?: GoToFunArgs<R, Q, PC, D>) => void>
+  : Record<N, (arg: GoToFunArgs<R, Q, PC, D>) => void>
 
 export type ReactComponentCreator<P> =
   | (new (props: P) => React.Component<any>)
